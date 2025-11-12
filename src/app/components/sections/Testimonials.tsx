a3381417dace2510382af9b0fb075bc90b609a28
Jack-Limas <jacklimassolarte456@gmail.com>
2025-11-12 08:33:31 -0500

    arreglando hydratation

--- src/app/components/sections/Testimonials.tsx
+++ src/app/components/sections/Testimonials.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import data from "@/app/data/data.json";
 import { useLocale } from "@/hooks/useLocale";
 import { useThemeMode } from "@/hooks/useTheme";
@@ -21,6 +21,12 @@ type TestimonialsData = {
 const Testimonials = () => {
   const { locale } = useLocale();
   const { mode } = useThemeMode();
+
+  // Protección hydration error:
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
   const t = (data as { [key: string]: { testimonials: TestimonialsData } })[locale].testimonials;
 
   return (
